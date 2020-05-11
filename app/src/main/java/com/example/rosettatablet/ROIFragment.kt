f62aa69450f13448dee02a336d1b6c9616bfb90e
Doron Atik <datik@controlrad.com>
2020-05-11 16:15:40 +0300

    git test

--- app/src/main/java/com/example/rosettatablet/ROIFragment.kt
+++ app/src/main/java/com/example/rosettatablet/ROIFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class ROIFragment : Fragment() {
-//Test
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_roi, container, false)
