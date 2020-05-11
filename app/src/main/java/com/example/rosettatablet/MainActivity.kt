9ad0a9dc69c7c36506313d1b8a335c0a8eced9f0
Doron Atik <datik@controlrad.com>
2020-05-11 15:27:18 +0300

    Added landscape layout; Added data binding

--- app/src/main/java/com/example/rosettatablet/MainActivity.kt
+++ app/src/main/java/com/example/rosettatablet/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.rosettatablet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.rosettatablet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.heyButton.setOnClickListener { Toast.makeText(this, "HEY", Toast.LENGTH_SHORT).show() }
     }
 }
