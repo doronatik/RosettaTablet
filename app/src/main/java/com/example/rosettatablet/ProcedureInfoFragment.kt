4c0a037fe3ad742da5f2cd6dd02e055330ff2f13
Doron Atik <datik@controlrad.com>
2020-05-12 15:09:09 +0300

    Added data binding to the fragments;

--- app/src/main/java/com/example/rosettatablet/ProcedureInfoFragment.kt
+++ app/src/main/java/com/example/rosettatablet/ProcedureInfoFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.rosettatablet.databinding.FragmentProcedureInfoBinding
 
 class ProcedureInfoFragment : Fragment() {
+    private lateinit var binding: FragmentProcedureInfoBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_procedure_info, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_procedure_info, container, false)
+        return  binding.root
     }
 }
--- app/src/main/java/com/example/rosettatablet/ROIFragment.kt
+++ app/src/main/java/com/example/rosettatablet/ROIFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.rosettatablet.databinding.FragmentRoiBinding
 
 class ROIFragment : Fragment() {
+    private lateinit var binding: FragmentRoiBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_roi, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_roi, container, false)
+        return  binding.root
     }
 }
